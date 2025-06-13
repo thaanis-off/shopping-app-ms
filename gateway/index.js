94f620717af317d0cba0cd0e56e83202d443c24b
mohamed_thaanis-amuryx <mohamed.thaanis@amuryx.com>
2025-06-13 23:20:23 +0530

    Changed Monolith to Microservice architecture

--- gateway/index.js
+++ gateway/index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/customer', proxy('http://localhost:8001'));
+app.use('/shopping', proxy('http://localhost:8003'));
+app.use('/', proxy('http://localhost:8002')); //products
 
 
 app.listen(8000, () => {
